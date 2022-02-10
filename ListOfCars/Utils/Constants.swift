450053cbd32dfed93ded07881ada6ff810426c92
Luis Eduardo <luis.eduardo.poa@hotmail.com>
2022-02-10 09:33:38 -0300

    Add Utils

---
+++ ListOfCars/Utils/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  ListOfCars
+//
+//  Created by Luis Eduardo Silva Oliveira on 09/02/22.
+//
+
+import Foundation
+
+// MARK: - Account Information
+let API_BASE = "https://desafioonline.webmotors.com.br/api/"
+
+// MARK: - Get Car
+let GET_MAKE = "OnlineChallenge/Make"
+let GET_MODEL = "OnlineChallenge/Model?MakeID="
+let GET_VERSION = "OnlineChallenge/Version?ModelID="
+let GET_VEHICLES = "OnlineChallenge/Vehicles?Page="
